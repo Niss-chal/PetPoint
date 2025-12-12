de65fa0e04db92a9f9fe312c5056dab45e1c9603
PranayaCodes <khadkapranaya14@gmail.com>
2025-12-12 08:40:54 +0545

    Verticaxl Scroll replaced with LazyColumn

--- app/src/main/java/com/project/petpoint/view/ProfileScreen.kt
+++ app/src/main/java/com/project/petpoint/view/ProfileScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,57 +33,82 @@ import com.project.petpoint.view.ui.theme.VividOrange
 fun ProfileScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
 
-        Column(
+            LazyColumn (
             modifier = Modifier
                 .fillMaxSize()
                 .background(Azure)
-                .padding(top = 50.dp)
-                .verticalScroll(rememberScrollState()),
+                .padding(top = 50.dp),
 
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+                contentPadding = PaddingValues(bottom = 20.dp)
         ) {
-            Spacer(modifier = Modifier.height(20.dp))
+           item {
+               Spacer(modifier = Modifier.height(20.dp))
+           }
 
+           item{
             Image(
                 painter = painterResource(id = R.drawable.profile),
                 contentDescription = null,
                 modifier = Modifier
                     .size(120.dp)
                     .clip(CircleShape)
-            )
+            )}
 
-            Spacer(modifier = Modifier.height(10.dp))
+            item {
 
-            Text(text = "Name", fontSize = 22.sp, color = Color.Black)
+                Spacer(modifier = Modifier.height(10.dp))
 
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Button(
-                onClick = { /* Navigate to Edit Profile */ },
-                shape = RoundedCornerShape(20.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = VividAzure),
-                modifier = Modifier.width(160.dp)
-            ) {
-                Text("Edit Profile", color = Color.White, fontSize = 16.sp)
+                Text(text = "Name", fontSize = 22.sp, color = Color.Black)
             }
 
-            Spacer(modifier = Modifier.height(40.dp))
+
+            item {
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Button(
+                    onClick = { /* Navigate to Edit Profile */ },
+                    shape = RoundedCornerShape(20.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = VividAzure),
+                    modifier = Modifier.width(160.dp)
+                ) {
+                    Text("Edit Profile", color = Color.White, fontSize = 16.sp)
+                }
+            }
 
 
-            Divider(
-                color = Color.Gray.copy(alpha = 0.3f),
-                thickness = 1.dp,
-                modifier = Modifier.padding(horizontal = 40.dp)
-            )
-
-            Spacer(modifier = Modifier.height(25.dp))
+            item {
 
 
-            ProfileMenuItem(icon = R.drawable.baseline_settings_24, text = "Settings")
+                Spacer(modifier = Modifier.height(40.dp))
+
+
+                Divider(
+                    color = Color.Gray.copy(alpha = 0.3f),
+                    thickness = 1.dp,
+                    modifier = Modifier.padding(horizontal = 40.dp)
+                )
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+            }
+            item {
+
+                ProfileMenuItem(icon = R.drawable.baseline_settings_24, text = "Settings")
+            }
+
+            item{
             ProfileMenuItem(icon = R.drawable.baseline_history_24, text = "Order History")
-            ProfileMenuItem(icon = R.drawable.baseline_logout_24, text = "Log Out")
+            }
 
-        }
+            item {
+                ProfileMenuItem(icon = R.drawable.baseline_logout_24, text = "Log Out")
+            }
+
+
+
+            }
 
 
         Row(
