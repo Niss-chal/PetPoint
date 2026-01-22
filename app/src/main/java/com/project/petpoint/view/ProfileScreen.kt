140c13608a8183b7e5394cc4442f7e5b302cc0f1
Niss-chal <240378@softwarica.edu.np>
2026-01-22 16:32:10 +0545

    Also login and delete account functionality added

--- app/src/main/java/com/project/petpoint/view/ProfileScreen.kt
+++ app/src/main/java/com/project/petpoint/view/ProfileScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
